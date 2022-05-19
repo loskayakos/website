fc9405c18afab8bca099ba7e623c2e774037c2cd
codersbros <p.grden@codersbros.pl>
2022-05-19 08:45:44 +0200

    create Hero in project

---
+++ src/componets/home/Hero.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Flex, Wrapper, Section } from '../shared/Index.styled'
+import HeroSlider from './HeroSlider'
+import HeroText from './HeroText'
+
+const Hero = () => {
+  return (
+    <>
+      <Section tabletPadding='0 0 130px'>
+        <Flex direction='column' tabletDirection='row'>
+          <HeroText />
+          <HeroSlider />
+        </Flex>
+      </Section>
+    </>
+  )
+}
+
+export default Hero
