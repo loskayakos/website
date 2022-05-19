621a5f6d465728f70996b715ec5cec9b8ca72d37
codersbros <p.grden@codersbros.pl>
2022-05-19 10:21:14 +0200

    remove warn webpack

--- gatsby-node.js
+++ gatsby-node.js
@@ -112,3 +112,34 @@ exports.createPages = async ({ actions, graphql, reporter }) => {
 //   `
 //   createTypes(typeDefs)
 // }
+exports.onCreateWebpackConfig = ({ stage, actions, getConfig, loaders, plugins }) => {
+  const config = getConfig()
+  const miniCssExtractPluginIndex = config.plugins.findIndex(
+    plugin => plugin.constructor.name === 'MiniCssExtractPlugin'
+  )
+
+  if (miniCssExtractPluginIndex > -1) {
+    // remove miniCssExtractPlugin from plugins list
+    config.plugins.splice(miniCssExtractPluginIndex, 1)
+
+    // re-add mini-css-extract-plugin
+    if (stage === 'build-javascript') {
+      config.plugins.push(
+        plugins.extractText({
+          filename: `[name].[contenthash].css`,
+          chunkFilename: `[name].[contenthash].css`,
+          ignoreOrder: true,
+        })
+      )
+    } else {
+      config.plugins.push(
+        plugins.extractText({
+          filename: `[name].css`,
+          chunkFilename: `[id].css`,
+          ignoreOrder: true,
+        })
+      )
+    }
+  }
+  actions.replaceWebpackConfig(config)
+}
