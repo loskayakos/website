2c18af2767b0770c216614e782826b3846d5b539
codersbros <p.grden@codersbros.pl>
2022-05-19 08:58:05 +0200

    create routing in config

--- src/config/routing.js
+++ src/config/routing.js
@@ -1,3 +1,14 @@
 export const routeLinks = {
-    
+  offer(slug) {
+    if (!slug) {
+      return `/oferta`
+    }
+    return `/oferta/${slug}`
+  },
+  raftingRoutes: '/trasy-splywow',
+  gallery: '/galeria',
+  contact: '/kontakt',
+  statute: '/regulamin',
+  cookiesPolicy: '/polityka-cookies',
+  privacyPolicy: '/polityka-prywatnosci',
 }
