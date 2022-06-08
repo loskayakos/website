5e33f216f9fd8a7c9954c2acf2cf0615f53dbc31
codersbros <p.grden@codersbros.pl>
2022-06-08 13:00:34 +0200

    change layout bookero

--- src/pages/rezerwacja.js
+++ src/pages/rezerwacja.js
@@ -10,6 +10,27 @@ const BookeroSection = styled(Section)`
   }
   && .submit-section .submit-button[data-v-78854db5] {
     background: ${variables.color.headerButton};
+    width: 100%;
+    height: 51px;
+    font-family: Manrope;
+    font-style: normal;
+    font-weight: 700;
+    font-size: 18px;
+    line-height: 150%;
+    background: rgb(82, 120, 44);
+    border-radius: 24px;
+    border: 2px solid transparent;
+    color: rgb(248, 249, 250);
+    transition: transform 0.5s cubic-bezier(0.345, 0.24, 0.07, 1) 0s;
+  }
+  && .submit-section .submit-button[data-v-78854db5]:active,
+  .submit-section .submit-button[data-v-78854db5]:focus,
+  .submit-section .submit-button[data-v-78854db5] {
+    &:hover {
+      background: rgb(255, 255, 255);
+      color: rgb(82, 120, 44);
+      border: 2px solid rgb(82, 120, 44);
+    }
   }
 `
 export default function Rezervation() {
