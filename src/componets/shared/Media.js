b61127747141c091ad55e0da03ec21579c30790a
codersbros <p.grden@codersbros.pl>
2022-07-07 11:11:22 +0200

    add Facebook and Instagram link to Media.js

--- src/componets/shared/Media.js
+++ src/componets/shared/Media.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useStaticQuery, graphql } from 'gatsby'
 import { Facebook } from '../icons/Facebook.icon'
 import { Instagram } from './../icons/Instagram.icon'
 import { InstagramBlue } from '../icons/InstagramBlue.icon'
@@ -6,13 +7,27 @@ import { FacebookBlue } from '../icons/FacebookBlue.icon'
 import { SectionTitle, Flex } from './Index.styled'
 
 const Media = ({ isBlueIcon, isContact }) => {
+  const data = useStaticQuery(graphql`
+    {
+      markdownRemark(frontmatter: { layout: { eq: "media" } }) {
+        frontmatter {
+          facebook
+          instagram
+        }
+      }
+    }
+  `)
+
+  const { markdownRemark } = data
+  const { frontmatter: page } = markdownRemark
+
   return (
     <Flex gap='22' items='center' tabletItems={isContact ? 'center' : 'flex-start'} content={isContact && 'center'}>
       {isContact ? '' : <SectionTitle color='#ffffff'>Media</SectionTitle>}
-      <a href=''>
+      <a href={`${page.facebook}`} target='_blank'>
         <figure>{isBlueIcon ? <FacebookBlue /> : <Facebook />}</figure>
       </a>
-      <a href=''>
+      <a href={`${page.instagram}`} target='_blank'>
         <figure>{isBlueIcon ? <InstagramBlue /> : <Instagram />}</figure>
       </a>
     </Flex>
