edfe243d76cd63f5837a0f52369af0abee95ab81
codersbros <p.grden@codersbros.pl>
2022-07-10 19:44:14 +0200

    update

---
+++ src/pages/regulaminy.js
@@ -0,0 +1,20 @@
+import React, { useEffect } from 'react'
+import Page from '../layout/Page'
+import styled from 'styled-components'
+import ContactForm from '../componets/shared/ContactForm'
+
+export default function Statute() {
+  return (
+    <Page>
+      <h1>Regulaminy</h1>
+      <a href={`Regulamin-kajaków.odt`} download>
+        Download the file that is already in your static folder
+      </a>
+      <div>
+        <a href={`Regulamin-kajaków.odt`} download>
+          Download the file that is already in your static folder
+        </a>
+      </div>
+    </Page>
+  )
+}
