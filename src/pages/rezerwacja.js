e9936479dbef88f8617e8628e4ad66eeb39479bc
codersbros <p.grden@codersbros.pl>
2022-07-12 18:34:54 +0200

    add ContactForm to Rezervation.js

--- src/pages/rezerwacja.js
+++ src/pages/rezerwacja.js
@@ -1,18 +1,14 @@
 import React, { useEffect } from 'react'
 import Page from '../layout/Page'
-import styled from 'styled-components'
 import { Section } from '../componets/shared/Index.styled'
-import variables from '../styles/variables'
+import ContactForm from './../componets/shared/ContactForm'
 
-const BookeroSection = styled(Section)`
-  display: flex;
-  flex-direction: column;
-  padding: 400px;
-`
 export default function Rezervation() {
   return (
     <Page>
-      <BookeroSection>{/* <ContactForm /> */}</BookeroSection>
+      <Section tabletPadding='160px 0 64px'>
+        <ContactForm />
+      </Section>
     </Page>
   )
 }
