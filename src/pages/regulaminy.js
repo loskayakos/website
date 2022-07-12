d323d43bdb8ab502071a9005115dc7a64ee30f41
codersbros <p.grden@codersbros.pl>
2022-07-12 19:56:37 +0200

    add statutes as pdf

--- src/pages/regulaminy.js
+++ src/pages/regulaminy.js
@@ -1,21 +1,35 @@
 import React, { useEffect } from 'react'
 import Page from '../layout/Page'
 import styled from 'styled-components'
+import { Flex, Section } from '../componets/shared/Index.styled'
+
+const DownloadLink = styled.a`
+  text-decoration: underline;
+  transition: transform 0.5s cubic-bezier(0.345, 0.24, 0.07, 1);
+  &:hover {
+    color: #52782c;
+    text-decoration: none;
+  }
+`
 
 export default function Statute() {
   return (
     <Page>
-      <h1>Regulaminy</h1>
-      <div>
-        <a href={'/Regulamin-kajaków.odt'} download>
-          / Download the file that is already in your static folder />
-        </a>
-      </div>
-      <div>
-        <a href={`/Regulamin-kajaków.odt`} download>
-          Download the file that is already in your static folder
-        </a>
-      </div>
+      <Section mobile='90px 20px 32px' tabletPadding='160px 5.5% 64px'>
+        <Flex direction='column' gap='48'>
+          <h1>Regulaminy</h1>
+          <div>
+            <DownloadLink href={'/Regulamin-kajaków.pdf'} download>
+              Regulamin kajaków
+            </DownloadLink>
+          </div>
+          <div>
+            <DownloadLink href={`/Regulamin-pola-namiotowego.pdf`} download>
+              Regulamin pola namiotowego
+            </DownloadLink>
+          </div>
+        </Flex>
+      </Section>
     </Page>
   )
 }
