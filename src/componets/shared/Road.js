c5b51e703e3489fb8d6b1d926f39eedb0abb21dc
codersbros <p.grden@codersbros.pl>
2022-07-13 20:48:09 +0200

    Update

---
+++ src/componets/shared/Road.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import { useStaticQuery, graphql } from 'gatsby'
+import styled from 'styled-components'
+
+const Select = styled.select`
+  background-color: white;
+  border: 1px solid #52782c;
+  border-radius: 4px;
+  font-size: 1rem;
+  line-height: 1.5rem;
+  font-style: normal;
+  font-weight: 400;
+  width: 100%;
+  margin-top: 0.5rem;
+  padding: 0.75rem 0.75rem;
+`
+
+const Road = ({ onChangeParent }) => {
+  const data = useStaticQuery(graphql`
+    {
+      allMarkdownRemark(filter: { frontmatter: { layout: { eq: "downflow-routes" } } }) {
+        edges {
+          node {
+            frontmatter {
+              title
+            }
+          }
+        }
+      }
+    }
+  `)
+  const { allMarkdownRemark } = data
+  const { edges } = allMarkdownRemark
+  const handleChange = e => {
+    onChangeParent(e.target.value)
+  }
+
+  return (
+    <Select name='road' component='select' onChange={handleChange}>
+      <option hidden> -- Wybierz swoją trasę spływu -- </option>
+      {edges.map(({ node }) => (
+        <option value={node.frontmatter.title} key={node.frontmatter.title}>
+          {' '}
+          {node.frontmatter.title}
+        </option>
+      ))}
+    </Select>
+  )
+}
+
+export default Road
