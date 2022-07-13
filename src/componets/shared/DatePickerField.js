45c704192988f65480149aab492c60d26be1b1f1
codersbros <p.grden@codersbros.pl>
2022-07-13 19:37:05 +0200

    updt

--- src/componets/shared/DatePickerField.js
+++ src/componets/shared/DatePickerField.js
@@ -1,24 +1,22 @@
 import React from 'react'
-import { Formik, useField, useFormikContext } from 'formik'
 import DatePicker, { registerLocale } from 'react-datepicker'
 import pl from 'date-fns/locale/pl' // the locale you want
 import 'react-datepicker/dist/react-datepicker.css'
 
 registerLocale('pl', pl) // register it with the name you want
 const DatePickerField = ({ ...props }) => {
-  const { setFieldValue } = useFormikContext()
-  const [field] = useField(props)
+  // const { setFieldValue } = useFormikContext()
   return (
     <DatePicker
-      {...field}
       {...props}
       placeholder='wybierz datę i godzinę spływu'
       locale='pl'
       showTimeSelect
-      selected={(field.value && new Date(field.value)) || null}
+      selected={new Date() || null}
       dateFormat='MMMM d, yyyy h:mm aa'
       onChange={val => {
-        setFieldValue(field.name, val)
+        console.log(val)
+        // setFieldValue(field.name, val)
       }}
     />
   )
--- src/layout/Footer.js
+++ src/layout/Footer.js
@@ -1,18 +1,29 @@
 import React from 'react'
-import { Flex, Section } from '../componets/shared/Index.styled'
+import { Flex, InternalLink, Section } from '../componets/shared/Index.styled'
 import Media from '../componets/shared/Media'
 import Company from '../componets/shared/Company'
 import Contact from '../componets/shared/Contact'
+import { Link } from 'gatsby'
+import { routeLinks } from './../config/routing'
 
 const Footer = () => {
   return (
-    <Section background='#272B30' mobile='48px 20px' tabletPadding='52px 5.5%'>
-      <Flex direction='column' tabletDirection='row' tabletContent='space-between'>
-        <Contact />
-        <Company />
-        <Media />
-      </Flex>
-    </Section>
+    <>
+      <Section background='#272B30' mobile='48px 20px' tabletPadding='52px 5.5%'>
+        <Flex direction='column' tabletDirection='row' tabletContent='space-between'>
+          <Contact />
+          <Company />
+          <Media />
+        </Flex>
+      </Section>
+
+      <Section mobile='0 0 48px' tabletPadding='0 0 52px'>
+        <Flex gap='16' direction='column' items='center' content='center' tabletDirection='row' tabletGap='24'>
+          © {new Date().getFullYear()} Los Kayakos. All rights reserved.{' '}
+          <InternalLink to={routeLinks.statute}>Regulaminy</InternalLink>
+        </Flex>
+      </Section>
+    </>
   )
 }
 export default Footer
